ebeba2082598fe0e27dc76f13de7d74f683397d6
Alex C <calex781@gmail.com>
2021-12-13 19:43:35 -0500

    Improved styling on the form and created some functions that will allow user to add a book to the library array.

--- scripts.js
+++ scripts.js
@@ -2,8 +2,31 @@ const addBookBtn = document.querySelector("#addBookBtn");
 const noBooksLabel = document.querySelector(".noBooksLabel");
 const addBookForm = document.querySelector(".addBookForm");
 
+// Empty book library
+let bookLibrary = ["no"];
+
+// Book constructor that takes parameters and sets their values
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+// Add new book created with constructor to the book library
+function addToLibrary(newBook) {
+  bookLibrary.push(newBook);
+}
+
+// Function checking if bookLibrary is empty
+function emptyLibrary() {
+  if (bookLibrary.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 addBookBtn.addEventListener("click", () => {
-  noBooksLabel.remove();
-  addBookForm.style.display = "grid";
+  addBookForm.style.display = "flex";
   addBookBtn.remove();
 });
